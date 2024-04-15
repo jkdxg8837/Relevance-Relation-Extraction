be112f4b32b05874e6807c65caac5ca4800659a6
BarryGu <49837737+jkdxg8837@users.noreply.github.com>
2024-04-15 19:41:26 +0800

    Update generate_topo.sh

--- scripts/generate_topo.sh
+++ scripts/generate_topo.sh
@@ -1,10 +1,10 @@
-raw_train_txt = "./wn18rr/train.txt"
-raw_test_txt = "./wn18rr/test.txt"
+raw_train_txt="./data/wn18rr/train.txt"
+raw_test_txt="./data/wn18rr/test.txt"
 # raw_train_txt = "./data/Miniprogram/train.txt"
 # raw_test_txt = "./data/Miniprogram/test.txt"
-output_dir = "./"
+output_dir="./data/wn18rr"
 
-python ./data/Miniprogram/topology/graph_composer_generator.py $raw_train_txt $raw_test_txt $output_dir
-python ./data/Miniprogram/topology/node2vec_code.py $output_dir
+python ./data/Miniprogram/topology/graph_composer_generator.py "$raw_train_txt" "$raw_test_txt" "$output_dir"
+python ./data/Miniprogram/topology/node2vec_code.py "$output_dir"
 # Alipay.com Inc.
 # Copyright (c) 2004-2023 All Rights Reserved.
